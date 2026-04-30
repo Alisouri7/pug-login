1cd3033742c8228f6f35747c43b02069ade9ee49
Ali Souri <souri7205@gmail.com>
2026-04-30 09:43:21 +0330

    edit controller

--- controllers/user.js
+++ controllers/user.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 
 exports.getRegisterPage = async (req, res) => {
     res.render('register')
+    return
 }
 
 exports.register = async (req, res) => {
@@ -15,22 +16,24 @@ exports.register = async (req, res) => {
         password: hash
     })
 
-    res.json(user)
+    return res.json(user)
 }
 
 
 exports.getLoginPage = async (req, res) => {
     res.render('login')
+    return
 }
 
 exports.login = async (req, res) => {
     const { username, password } = req.body;
-
+    
 
     if (!username || !password) {
         res.render('login', {
             error: {message: 'Fill username and password'}
         })
+        return
     }
     const user = await userModel.findOne({ username }).lean();
 
@@ -38,6 +41,7 @@ exports.login = async (req, res) => {
         res.render('login', {
             error: { message: 'THIS USER IS NOT EXIST IN DATA BASE' }
         })
+        return
     }
 
 
@@ -47,10 +51,12 @@ exports.login = async (req, res) => {
         res.render('login', {
             error: { message: 'USERNAME OR PASSWORD IS WRONG' }
         })
+        return
     }
 
     res.render('mainpage', {
         user: { username: `${user.username}` }
     })
+    return
 
 }
